527900910a19e76b10e7c829a324267133f956f8
SakshiChak <mailme.sakshichak@gmail.com>
2025-07-21 19:05:20 +0530

    .

--- client/src/components/admin/CommentTableItem.jsx
+++ client/src/components/admin/CommentTableItem.jsx
@@ -6,12 +6,11 @@ import toast from "react-hot-toast";
 const CommentTableItem = ({ comment, fetchComments }) => {
   const { blog, createdAt, _id } = comment;
   const BlogDate = new Date(createdAt);
-
   const { axios } = useAppContext();
 
-  const approveComment = async()=>{
+  const approveComment = async () => {
     try {
-      const { data } = await axios.post('/api/admin/approve-comment', { id: _id }); 
+      const { data } = await axios.post('/api/admin/approve-comment', { id: _id });
       if (data.success) {
         toast.success(data.message);
         await fetchComments();
@@ -23,12 +22,12 @@ const CommentTableItem = ({ comment, fetchComments }) => {
     }
   }
 
-  const deleteComment = async()=>{
+  const deleteComment = async () => {
     try {
       const confirm = window.confirm('Are you sure you want to delete this comment?');
       if (!confirm) return;
 
-      const { data } = await axios.post('/api/admin/delete-comment', { id: _id }); 
+      const { data } = await axios.post('/api/admin/delete-comment', { id: _id });
       if (data.success) {
         toast.success(data.message);
         await fetchComments();
